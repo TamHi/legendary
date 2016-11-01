d24a49dd6aafd3b0410cbc8f49d0f3b0f321ee8c
Tam Bui <Tam Bui>
2016-11-01 07:09:42 +0700

    Add ng2-material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { Ng2MaterialModule } from 'ng2-material';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
